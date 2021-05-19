92e0163467b1e0b81e3a7b1eeaa0a4bed9b0e8f2
Mubassir-Ali <mubassir11@gmail.com>
2021-05-19 13:08:50 +0500

    add header of Todos App

--- App.js
+++ App.js
@@ -8,41 +8,28 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-const Data = [
-  { name: "ali", id: "1" },
-  { name: "bilal", id: "2" },
-  { name: "haris", id: "3" },
-  { name: "hamad", id: "4" },
-  { name: "Mohsin", id: "5" },
-  { name: "Usman", id: "6" },
-  { name: "Inzamam", id: "7" },
-  { name: "Yasir", id: "8" },
+import Header from './components/Header'
+
+const todosData = [
+  { text: "buy coffee", key: "1" },
+  { text: "create an app", key: "2" },
+  { text: "play on switch", key: "3" },
 ];
 
 export default function App() {
-  
-
-  const [person, setPerson] = useState(Data);
-const pressHandler=(id)=>{
-  console.log(id);
-  setPerson((prevPerson)=>{
-    return prevPerson.filter(person=>person.id !=id)
-  })
-}
+  const [todos, setTodos] = useState(todosData);
   return (
     <View style={styles.container}>
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data={person}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={()=>pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-
-      <StatusBar style="auto" />
+      <Header/>
+      <View style={styles.content}>
+        {/* todo Form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -51,18 +38,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
-    paddingTop: 40,
-    paddingHorizontal: 20,
   },
-  item: {
-    backgroundColor: "pink",
-    padding: 30,
-    marginTop: 24,
-    fontSize: 24,
-    textAlign: "center",
-    borderRadius: 12,
-    marginHorizontal: 24,
+  content: {
+    padding: 40,
+    
   },
+  list:{
+    marginTop:24
+  }
 });
