94c926210e8a8e1385baee84365035676e497a16
Mubassir-Ali <mubassir11@gmail.com>
2021-05-19 11:34:20 +0500

    add List and ScrollView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 const Data = [
   { name: "ali", key: 1 },
@@ -16,34 +16,20 @@ const Data = [
 export default function App() {
   const [person, setPerson] = useState({ name: "Mubassir", age: 28 });
 
-  const [studentName, setStudentName]=useState(Data)
+  const [studentName, setStudentName] = useState(Data);
 
   return (
     <View style={styles.container}>
-      <Text>Enter Your Name</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="e.g Ali"
-        onChangeText={(val) => setPerson({ name: val, age: person.age })}
-      />
 
-      <Text>Enter your Age</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="e.g 27"
-        keyboardType="numeric"
-        onChangeText={(val) => setPerson({ age: val, name: person.name })}
-      />
+    <ScrollView>
+    {studentName.map((item) => (
+        <View key={item.key}>
+          <Text style={styles.item}>{item.name}</Text>
+        </View>
+      ))}
 
-      <Text>
-        My name is {person.name}, I am {person.age} year old{" "}
-      </Text>
-
-
-
-
-
-
+    </ScrollView>
+  
 
       <StatusBar style="auto" />
     </View>
@@ -54,19 +40,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    // paddingTop:40,
-    // paddingHorizontal:20
+    // alignItems: "center",
+    // justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
   },
-
-  input: {
-    borderWidth: 1,
-    borderColor: "#999",
-    borderRadius: 8,
-    width: 200,
-    paddingHorizontal: 8,
-    paddingVertical: 2,
-    textAlign: "center",
+  item: {
+    backgroundColor: "pink",
+    padding:30,
+    marginTop:24,
+    fontSize:24,
+    textAlign:"center",
+    borderRadius:12
+    
   },
 });
